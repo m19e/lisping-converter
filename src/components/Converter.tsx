42a0e2844e146bdc924aa3fc930d026b29464db2
m19e <nexus.me7290@gmail.com>
2020-12-23 00:06:56 +0900

    Set state, click func

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,16 +1,40 @@
+import React, { useState } from "react";
+
 export default function Converter() {
+    const [input, setInput] = useState("");
+    const [output, setOutput] = useState("");
+
+    const onInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setInput(e.currentTarget.value);
+    };
+
+    const convert = () => {
+        setOutput(input);
+    };
+
     return (
         <>
             <div className="flex-grow flex justify-center items-center p-4">
-                <textarea placeholder="テキストを入力" className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"></textarea>
+                <textarea
+                    onChange={onInputChange}
+                    placeholder="テキストを入力"
+                    className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"
+                ></textarea>
             </div>
             <div className="flex justify-center items-center">
-                <button className="inline-block px-6 py-2 text-md font-medium leading-6 text-center text-black uppercase transition bg-gray-100 rounded-full shadow ripple hover:shadow-lg hover:bg-gray-200 focus:outline-none">
+                <button
+                    onClick={() => convert()}
+                    className="inline-block px-6 py-2 text-md font-medium leading-6 text-center text-black uppercase transition bg-gray-100 rounded-full shadow ripple hover:shadow-lg hover:bg-gray-200 focus:outline-none"
+                >
                     変換
                 </button>
             </div>
             <div className="flex-grow flex justify-center items-center p-4">
-                <textarea placeholder="変換されたテキスト" className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"></textarea>
+                <textarea
+                    value={output}
+                    placeholder="変換されたテキスト"
+                    className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"
+                ></textarea>
             </div>
         </>
     );
