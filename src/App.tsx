3e4277533fcb6f9ec2531938269207e0e91b3ced
m19e <nexus.me7290@gmail.com>
2020-12-23 02:44:37 +0900

    Small size h1 base

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <div className="flex flex-col w-screen h-screen bg-gray-200">
             <div className="flex justify-center items-center">
-                <h1 className="m-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">舌足らず変換ツール</h1>
+                <h1 className="m-2 text-2xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">舌足らず変換ツール</h1>
             </div>
             <Converter />
         </div>
