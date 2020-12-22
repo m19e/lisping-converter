c62d5d9fc2c493171d3967b8b1ac8de7c6d4bce8
m19e <nexus.me7290@gmail.com>
2020-12-22 21:25:34 +0900

    Set flex column

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function App() {
     return (
-        <div className="w-screen h-screen bg-gray-200">
+        <div className="flex flex-col w-screen h-screen bg-gray-200">
             <div className="flex justify-center items-center">
                 <h1 className="m-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">舌足らず変換ツール</h1>
             </div>
