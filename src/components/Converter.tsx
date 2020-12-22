5fd8bbd858caebb5fd5268d36dca08fcaab88654
m19e <nexus.me7290@gmail.com>
2020-12-22 23:56:28 +0900

    Set placeholder

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -2,7 +2,7 @@ export default function Converter() {
     return (
         <>
             <div className="flex-grow flex justify-center items-center p-4">
-                <textarea className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"></textarea>
+                <textarea placeholder="テキストを入力" className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"></textarea>
             </div>
             <div className="flex justify-center items-center">
                 <button className="inline-block px-6 py-2 text-md font-medium leading-6 text-center text-black uppercase transition bg-gray-100 rounded-full shadow ripple hover:shadow-lg hover:bg-gray-200 focus:outline-none">
@@ -10,7 +10,7 @@ export default function Converter() {
                 </button>
             </div>
             <div className="flex-grow flex justify-center items-center p-4">
-                <textarea className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"></textarea>
+                <textarea placeholder="変換されたテキスト" className="h-full w-full max-w-4xl rounded-md resize-none focus:outline-none"></textarea>
             </div>
         </>
     );
